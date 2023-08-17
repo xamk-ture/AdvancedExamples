d1b6cfc3b5091300f27113a61e78fb2f2dd2dea0
Tuomas Reijonen <hture01@xamk.fi>
2023-08-17 17:46:02 +0300

    try catch example added

---
+++ TryCatchExample/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace TryCatchExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Syötä kokonaisluku:");
+
+            // Luetaan syöte käyttäjältä.
+            string userInput = Console.ReadLine();
+
+            try
+            {
+                // Yritetään muuttaa syöte kokonaisluvuksi.
+                int number = int.Parse(userInput);
+                Console.WriteLine($"Syöttämäsi luku on: {number}");
+            }
+            catch (FormatException exception)
+            {
+                // Tämä osio suoritetaan, jos yllä oleva koodi aiheuttaa FormatException-virheen.
+                Console.WriteLine("Annettu syöte ei ollut kelvollinen kokonaisluku.");
+
+                //exception muuttujasta voidaan löytää tarkempi syy miksi ohjelma kaatui
+                Console.WriteLine(exception.Message);
+
+                //Muista, että muuttuja sisältää monia muitai tietoja, josta saa vieläkin tarkempaa tietoa, kuten stack trace
+                Console.WriteLine(exception.StackTrace);
+            }
+            finally // finally osio on valinnainen
+            {
+                // Tämä osio suoritetaan aina, riippumatta siitä, tuliko virheitä vai ei.
+                Console.WriteLine("Tämä suoritetaan aina!");
+            }
+        }
+    }
+}
